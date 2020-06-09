94d525f4e9a63ad4631408adeb59d1ac0f77f442
annavoitovich <anya.voitovich@icloud.com>
2020-06-09 12:20:21 +0300

    ПЕРВЫЙ ПРОЕКТ

---
+++ habrclone/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+App = Flask(__name__)
+
+from habrclone import routes
---
+++ habrclone/routes.py
@@ -0,0 +1,7 @@
+from habrclone import App
+from flask import render_template
+
+@App.route('/')
+@App.route('/index')
+def index():
+    return render_template('index.html')
